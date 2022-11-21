5320c3a287b30cfb731bdbf880f8cf849357e32b
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2022-11-21 23:51:42 +0900

    ✨ 각 탭에서 쓸 페이지 구현 (#1)

---
+++ pages/tabs/history.tsx
@@ -0,0 +1,3 @@
+export default function History() {
+  return <div>기록페이지</div>;
+}
---
+++ pages/tabs/setting.tsx
@@ -0,0 +1,8 @@
+export default function Album() {
+  return (
+    <div>
+      설<br />정<br />
+      페이지
+    </div>
+  );
+}
